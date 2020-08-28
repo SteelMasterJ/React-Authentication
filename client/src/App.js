30a04f25b2db6824be7daa6a05e753704be273a2
Joseph Steel <joseph.steel.iv@gmail.com>
2020-08-28 15:02:07 -0400

    minor updates

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,9 @@ import UserSignIn from './components/UserSignIn';
 import UserSignOut from './components/UserSignOut';
 import Authenticated from './components/Authenticated';
 
+import withContext from './Context';
+const UserSignUpWithContext = withContext(UserSignUp);
+
 export default () => (
   <Router>
     <div>
@@ -22,7 +25,7 @@ export default () => (
         <Route exact path="/" component={Public} />
         <Route path="/authenticated" component={Authenticated} />
         <Route path="/signin" component={UserSignIn} />
-        <Route path="/signup" component={UserSignUp} />
+        <Route path="/signup" component={UserSignUpWithContext} />
         <Route path="/signout" component={UserSignOut} />
         <Route component={NotFound} />
       </Switch>
